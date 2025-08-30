bfe39ee8d0332444a866deee34293194dadc9fbc
Santiago Hoyos <santiagojhoyoss@gmail.com>
2025-08-29 23:26:16 -0500

    done taller 2

--- taller-02.js
+++ taller-02.js
@@ -1,23 +1,62 @@
-function findMax(list){
-    let max = list[0]
-    for (const item of list){
-        if (item > max){
-            max = item
-        }
+function findMax(list) {
+  let max = list[0];
+  for (const item of list) {
+    if (item > max) {
+      max = item;
     }
-    return max              
+  }
+  return max;
 }
 
-const test = [3, 17, -1, 4, -19]
+const test = [3, 17, -1, 4, -19];
 // console.log(findMax(test))
 
-function includes(list, num){
-    for (const item of list){
-        if (num === item){
-            return true
-        }
+function includes(list, num) {
+  for (const item of list) {
+    if (num === item) {
+      return true;
     }
-    return false
+  }
+  return false;
 }
 
-console.log(includes(test, 4))
+// console.log(includes(test, 4))
+
+function sum(list) {
+  let total = 0;
+  for (const item of list) {
+    total += item;
+  }
+  return total;
+}
+
+// console.log(sum(test));
+
+const test2 = [7, 2, 4, 6, 3, 9];
+
+function missingNumbers(list) {
+  let faltantes = [];
+  let temp;
+  for (let i = 0; i < list.length; i++) {
+    for (let j = 0; j < list.length - 1 - i; j++) {
+      if (list[j] > list[j + 1]) {
+        temp = list[j];
+        list[j] = list[j + 1];
+        list[j + 1] = temp;
+      }
+    }
+  }
+
+  let c = 0;
+  for (let i = list[0]; i <= list[list.length - 1]; i++) {
+    if (list[c] !== i) {
+      faltantes.push(i);
+    } else {
+      c++;
+    }
+  }
+  return faltantes;
+}
+
+console.log(missingNumbers(test2));
+
