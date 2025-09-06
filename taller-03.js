1a5d02d1dd6e9838150161fbb8b8e24b28b83f9b
Santiago Hoyos <santiagojhoyoss@gmail.com>
2025-09-05 23:46:16 -0500

    done

--- taller-03.js
+++ taller-03.js
@@ -40,10 +40,57 @@ function filterConsonantes(letter) {
 }
 
 function desglosarString(string, command) {
-  return string.split("").filter((letter => command === "vocales" ? filterVocales(letter) : filterConsonantes(letter))).length;
+  return string
+    .split("")
+    .filter((letter) =>
+      command === "vocales" ? filterVocales(letter) : filterConsonantes(letter)
+    ).length;
 }
 
-console.log(desglosarString("murcielagos", "vocales"));
-
+// console.log(desglosarString("murcielagos", "vocales"));
 
 // PUNTO 2
+
+function twoSum(numbers, target) {
+  let hashmap = new Map();
+  let complement, x;
+  for (let i = 0; i < numbers.length; i++) {
+    complement = target - numbers[i];
+    if (hashmap.has(complement)) {
+      return [hashmap.get(complement), i];
+    }
+    hashmap.set(numbers[i], i);
+  }
+  return [];
+}
+
+// console.log(twoSum([3, 4, 2], 6));
+
+// PUNTO 3
+function conversionRomana(roman) {
+  const ROMAN_VALUES = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+  for (let i = 0; i < roman.length; i++) {
+    const curr = ROMAN_VALUES[roman[i]];
+    const next = ROMAN_VALUES[roman[i + 1]] ?? 0;
+
+    if (curr < next) {
+      total += next - curr;
+      i++;
+    } else {
+      total += curr;
+    }
+  }
+  return total;
+}
+
+console.log(conversionRomana("MCMXCVII")); 
