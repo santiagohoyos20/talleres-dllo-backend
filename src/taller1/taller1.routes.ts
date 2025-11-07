ff067383e7ca7e5ddac50b4af728bb0ae51cc902
Santiago Hoyos <santiagojhoyoss@gmail.com>
2025-11-07 02:19:50 -0500

    manejo de errores

--- src/taller1/taller1.routes.ts
+++ src/taller1/taller1.routes.ts
@@ -6,24 +6,45 @@ import { convertidorTemp, resolvedor, mejorParidad, peorParidad } from "./taller
 const taller1Routes = Router();
 
 // DECLARE ENDPOINT FUNCTIONS
-async function convertidorTemperatura(request: Request, response: Response) {
-    const { celsius } = request.body;
-    const fahrenheit = convertidorTemp(celsius);
-
-    response.status(200).json({
-        message: "Success.",
-        fahrenheit: fahrenheit,
-    });
+function convertidorTemperatura(request: Request, response: Response) {
+    try {
+        const { celsius } = request.body;
+        if (typeof celsius !== "number") {
+            throw new Error("Invalid input.");
+        }
+        const fahrenheit = convertidorTemp(celsius);
+        response.status(200).json({
+            message: "Success.",
+            fahrenheit: fahrenheit,
+        });
+    } catch (error: any) {
+        response.status(500).json({
+            message: "Error.",
+            error: error.message,
+        });
+    }
 }
 
 async function postResolvedor(request: Request, response: Response) {
-    const { a, b, c, signo } = request.body;
-    const resultado = resolvedor(a, b, c, signo);
+    try {
+        const { a, b, c, signo } = request.body;
+        const resultado = resolvedor(a, b, c, signo);
 
-    response.status(200).json({
-        message: "Success.",
-        resultado: resultado,
-    });
+        if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number" || (signo !== 1 && signo !== -1)) {
+            throw new Error("Invalid input.");
+        }
+
+        response.status(200).json({
+            message: "Success.",
+            resultado: resultado,
+        });
+    } catch (error: any) {
+        response.status(500).json({
+            message: "Error.",
+            error: error.message,
+        });
+    }
+
 }
 
 async function postMejorParidad(request: Request, response: Response) {
